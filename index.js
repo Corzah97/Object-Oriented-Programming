de4e048044e32aaadcfa417b04ccc24b079dffb6
Corey Boughton <corzah911@gmail.com>
2023-04-25 10:00:33 +1000

    testing jest

--- index.js
+++ index.js
@@ -16,28 +16,28 @@ function writeToFile(fileName, answers) {
   let svgString = "";
   // Sets width and height of logo container
  svgString =
-    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"/>';
   
-    svgString += "<g>";
+    // svgString += "<g>";
   
     svgString += `${answers.shape}`;
 
   let shapeChoice;
   if (answers.shape === "Triangle") {
     shapeChoice = new Triangle();
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    svgString += `<polygon height=“100%” width=“100%” points=“0,200 300,200 150,0” fill=“${answers.color}“/>`;
   } else if (answers.shape === "Square") {
     shapeChoice = new Square();
-    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    svgString += `<rect x=“50” height=“200" width=“200” fill=“${answers.color}“/>`;
   } else {
     shapeChoice = new Circle();
-    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    svgString += `<circle cx=“50%” cy=“50%” r=“100” height=“100%” width=“100%” fill='${answers.color}'/>`;
   }
 
  
   svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
   // Closing </g> tag
-  svgString += "</g>";
+//   svgString += "</g>";
   // Closing </svg> tag
   svgString += "</svg>";
 
